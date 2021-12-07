d2617bc9bec2686e6849161da7592ababc16d84d
Antoine Jamelot <antoine.jamelot@outlook.fr>
2021-12-07 12:22:06 +0100

    Implemented some methods of GraphInterface

--- src/Graph.java
+++ src/Graph.java
@@ -68,7 +68,87 @@ public abstract class Graph implements GraphInterface {
 		
 		return copy;
 	}
+
+	public Set<Edge> edges()
+	{
+		Set<Edge> copy = new HashSet<Edge>();
+		for ( Edge e : edges )
+		{
+			copy.add(new Edge(e.source(), e.destination(), e.flow(), e.capacity()));
+		}
+		return copy;
+	}
+
+	public Graph addNode(Integer node)
+	{
+		nodes.add(node);
+		return this;
+	}
+
+	public Graph removeNode(Integer node)
+	{
+		nodes.remove(node);
+		return this;
+	}
+
+	public Graph addEdge(Edge e)
+	{
+		edges.add(e);
+		return this;
+	}
+
+	public Graph addEdge(Integer nodeU, Integer nodeV)
+	{
+		edges.add(new Edge(nodeU, nodeV));
+		return this;
+	}
+
+	public Graph addEdge(Integer nodeU, Integer nodeV, Integer capacity)
+	{
+		edges.add(new Edge(nodeU, nodeV, capacity));
+		return this;
+	}
+
+	public Graph addEdge(Integer nodeU, Integer nodeV, Integer flow, Integer capacity)
+	{
+		edges.add(new Edge(nodeU, nodeV, flow, capacity));
+		return this;
+	}
 	
+	public Graph removeEdge(Edge e)
+	{
+		edges.remove(e);
+		return this;
+	}
+	
+	public Graph removeEdge(Integer nodeU, Integer nodeV)
+	{
+		for ( Edge e : edges )
+			if ( e.source() == nodeU && e.destination() == nodeV )
+			{
+				edges.remove(e);
+			}
+		return this;
+	}
+
+	public Iterator<Integer> nodeIterator()
+	{
+		return nodes.iterator();
+	}
+		
+	public Iterator<Edge> edgeIterator()
+	{
+		return edges.iterator();
+	}
+	
+	public Edge getEdge(Integer nodeU, Integer nodeV)
+	{
+		for ( Edge e : edges )
+			if ( e.source() == nodeU && e.destination() == nodeV )
+				return e;
+		return null;
+	}
+
 	public Boolean isDirected()
 	{
 		return this.directed;
